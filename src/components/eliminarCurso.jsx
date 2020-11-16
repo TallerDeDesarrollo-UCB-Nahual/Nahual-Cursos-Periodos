8b6142e0f8f1debb14d9c6035ab89e849d1c24c7
Luis Toro <luis.toro.flores97@gmail.com>
2020-11-16 19:34:13 -0400

    redireccionar cuando elianmos un curso

--- src/components/eliminarCurso.jsx
+++ src/components/eliminarCurso.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-//import { Button, Confirm } from "semantic-ui-react";
-//import 'semantic-ui-css/semantic.min.css'
 import { Button } from "shards-react";
 import { eliminarCursoDePeriodo } from "../servicios/periodos";
-import {useHistory} from "react-router-dom"
-import { Link } from "react-router-dom"
 
 function EliminarCurso({ idPeriodo, idCurso }) {
-    const history = useHistory();
 
     const eliminarCursoDeAPI = (idPeriodo, idCurso) => {
         eliminarCursoDePeriodo(idPeriodo, idCurso);
-        <Link to="/periodos"></Link>
-        history.push("/periodos")
+        window.location.href = "http://localhost:3000/periodos";
     }
 
     return (
