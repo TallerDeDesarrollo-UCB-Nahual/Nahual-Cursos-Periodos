00e498f46afbab115c17f44c668baff9bd8a9e05
Diego <DiegoViscarra@users.github.com>
2020-11-27 11:18:16 -0400

    Cambios parciales de front

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Periodos from "./components/periodos";
 import NuevoPeriodo from "./components/crearperiodo";
 import EditarPeriodo from "./components/editarperiodo";
+import ListaNodosSedes from "./components/listaNodos-Sedes";
+import TabExamplePointing from "./components/tabsgenerales";
 
 function App() {
   return (
@@ -16,6 +18,12 @@ function App() {
           </Route>
           <Route exact path="/formulario-registro-periodo">
             <NuevoPeriodo />
+            </Route>
+          <Route exact path="/tabs">
+            <TabExamplePointing/>
+          </Route>
+          <Route exact path="/nodos-sedes">
+            <ListaNodosSedes />
           </Route>
           <Route path="/periodos/:id" children={<EditarPeriodo />} />
           <Route path="/">no estas permitido</Route>
