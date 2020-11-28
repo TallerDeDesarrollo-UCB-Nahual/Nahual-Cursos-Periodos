5a1ffcef86faf4a7ac01cee9fa97293726bb35a3
Jose Carlos <josecarlos.jcop.3@gmail.com>
2020-11-28 16:09:23 -0400

    Dropdown terminado
    Se completo el formato de envio de estos

--- src/components/Alumnes/ModalCambioEstado.jsx
+++ src/components/Alumnes/ModalCambioEstado.jsx
@@ -1,72 +1,99 @@
-import React from 'react'
-import { Button, Header, Modal, Dropdown } from 'semantic-ui-react'
-
-function ModalCambioEstado() {
-  const [open, setOpen] = React.useState(false)
-  const [opcionSeleccionada, setOpcionSeleccionada] = React.useState({estadoId: 5,
-  nombre: "Egresade"})
-  
-  const stateOptions = [
-  { key: '1', text: 'PreInscripte', value: 'PreInscripte' },
-  { key: '3', text: 'Abandonade', value: 'Abandonade' },
-  { key: '5', text: 'Egresade', value: 'Egresade' }
-]
-  
-  function opcionSeleccionado(option){
-    setOpcionSeleccionada({
-        estadoId: stateOptions.key,
-        nombre: stateOptions.value
-    })
-  }
-
-  
-  return (
-    <Modal
-      size="tiny"
-      onClose={() => setOpen(false)}
-      onOpen={() => setOpen(true)}
-      open={open}
-      trigger={<Button>Cambiar Estado</Button>}
-    >
-    {console.log(opcionSeleccionada)}
-      <Modal.Header>Cambio de Estado</Modal.Header>
-      <Modal.Content style={{textAlign:"center"}}>
-        <Modal.Description>
-        <Dropdown          
-          text={opcionSeleccionada.value} 
-          placeholder= 'Seleccionar curso'
-          defaultValue={stateOptions[1].value}
-        >
-        <Dropdown.Menu defaultValue="Egresade">
-          {stateOptions.map((option) => (
-            <Dropdown.Item 
-            key={option.key}
-            value={option.value}
-            text={option.text}
-            {... option}
-            onClick={() => this.opcionSeleccionado(option)}
-            />
-          ))}
-        </Dropdown.Menu>
-        </Dropdown>
-        
-        
-        </Modal.Description>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button onClick={() => setOpen(false)}>
-          Cancel
-        </Button>
-        <Button
-          content="Confirmar Cambio"
-          labelPosition='right'
-          icon='checkmark'
-          positive
-        />
-      </Modal.Actions>
-    </Modal>
-  )
+import React from "react";
+import { Button, Modal, Dropdown } from "semantic-ui-react";
+import Axios from "axios";
+function ModalCambioEstado(props) {
+	const [open, setOpen] = React.useState(false);
+	const opciones = [
+		{ key: "1", text: "PreInscripte", value: "PreInscripte" },
+		{ key: "3", text: "Abandonade", value: "Abandonade" },
+		{ key: "5", text: "Egresade", value: "Egresade" }
+	];
+	const [opcionSeleccionada, setOpcionSeleccionada] = React.useState(
+		opciones[2].value
+	);
+	const handleChange = (e, { value }) => setOpcionSeleccionada(value);
+	const prepararDatos = () => {
+		let estadoACambiar;
+		let estudiantes = [];
+		switch (opcionSeleccionada) {
+			case "PreInscripte":
+				estadoACambiar = 1;
+				break;
+			case "Abandonade":
+				estadoACambiar = 3;
+				break;
+			case "Egresade":
+				estadoACambiar = 5;
+				break;
+			default:
+				estadoACambiar = 2;
+				break;
+		}
+		props.alumnes.map((alumne) => {
+			return estudiantes.push(alumne.estudiante);
+		});
+		const datos = JSON.stringify({
+			estudiates: estudiantes,
+			estado: estadoACambiar
+		});
+		return datos;
+	};
+	const cambiarEstado = () => {
+		const API_URL = process.env.REACT_APP_API_URL;
+		console.log(prepararDatos());
+		// Axios.post({
+		// 	method: "post",
+		// 	url: `${API_URL}/estudiantes/cambiarEstadoAlumnes/`,
+		// 	headers: { "Content-Type": "application/json" },
+		// 	data: prepararDatos()
+		// }).then((response) => {
+		// 	console.log(response.data);
+		// })
+		var headers = {
+			"Content-Type": "application/json"
+		};
+		Axios.post(
+			`${API_URL}/estudiantes/cambiarEstadoAlumnes/`,
+			prepararDatos(),
+			{ headers: headers }
+		).then((response) => {
+			console.log("reactNativeDemo", "response get details:" + response.data);
+		});
+	};
+	return (
+		<Modal
+			size="tiny"
+			onClose={() => setOpen(false)}
+			onOpen={() => setOpen(true)}
+			open={open}
+			trigger={<Button>Cambiar Estado</Button>}
+		>
+			{console.log(opcionSeleccionada)}
+			{console.log(props.alumnes)}
+			<Modal.Header>Cambio de Estado</Modal.Header>
+			<Modal.Content style={{ textAlign: "center" }}>
+				<Modal.Description>
+					<Dropdown
+						onChange={handleChange}
+						options={opciones}
+						placeholder="Elija una opcion"
+						selection
+						value={opcionSeleccionada}
+					/>
+				</Modal.Description>
+			</Modal.Content>
+			<Modal.Actions>
+				<Button onClick={() => setOpen(false)}>Cancel</Button>
+				<Button
+					content="Confirmar Cambio"
+					labelPosition="right"
+					icon="checkmark"
+					positive
+					onClick={() => cambiarEstado()}
+				/>
+			</Modal.Actions>
+		</Modal>
+	);
 }
 
-export default ModalCambioEstado
-
+export default ModalCambioEstado;
