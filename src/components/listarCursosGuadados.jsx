0bc44bddfb57aafc4f717c5b9587c27b93455872
Miguel Campero <miguelcamp@hotmail.com>
2020-11-28 21:02:26 -0400

    Cambio de buttons a tags

--- src/components/listarCursosGuadados.jsx
+++ src/components/listarCursosGuadados.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Modal, Header, Advertisement, Button } from 'semantic-ui-react';
 import EliminarCurso from "./eliminarCurso";
+import { Icon, Label } from 'semantic-ui-react'
 
 export default function ListarCursosGuardados({cursos, estaAbierto, setAbierto, idPeriodo}) {
     console.log(cursos)
@@ -10,10 +11,11 @@ export default function ListarCursosGuardados({cursos, estaAbierto, setAbierto, 
             return (
             <Advertisement>
                 {c.horarioInicio} - {c.horarioFin} - {" "}
-                <Button color="blue" size="tiny">Nodo: {c.nodo.nombre}</Button>{" "}
-                <Button color="teal" size="tiny">Sede: {c.sede.nombre}</Button>{" "}
-                <Button color="yellow" size="tiny">Profesores: {c.profesores}</Button>
-                <Button color="violet" size="tiny">Notas: {c.notas}</Button>
+                <Label color="blue" size="tiny">Nodo: {c.nodo.nombre}</Label>{" "}
+                <Label color="teal" size="tiny">Sede: {c.sede.nombre}</Label>{" "}
+                <Label color="yellow" size="tiny">Profesores: {c.profesores}</Label>
+                <Label color="violet" size="tiny">Notas: {c.notas}</Label>
+                
                 <EliminarCurso idPeriodo={idPeriodo} idCurso={c.id}></EliminarCurso>
             </Advertisement>)
         })}
