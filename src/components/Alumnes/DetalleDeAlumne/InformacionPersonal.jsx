edcc1dcc87bd1a7aadad1b1a140149a9f0c01c94
FidelRojas <45080814+FidelRojas@users.noreply.github.com>
2020-11-30 22:43:29 -0400

    correccion de espacio

--- src/components/Alumnes/DetalleDeAlumne/InformacionPersonal.jsx
+++ src/components/Alumnes/DetalleDeAlumne/InformacionPersonal.jsx
@@ -22,40 +22,33 @@ function InformacionPersonal({ alumne }) {
         <Item.Content verticalAlign="middle">
           <div style={{ textAlign: "left" }}>
             <Header as="h1">
-              {" "}
-              {convertirAMayusculas(alumne.nombre + alumne.apellido)}{" "}
+              {convertirAMayusculas(alumne.nombre + " " + alumne.apellido)}
             </Header>
             <br />
             <Item.Description>
               <p>
-                {" "}
                 <Icon name="mail outline" /> <b>Correo: </b>
                 {alumne.correo}
               </p>
               <p>
-                {" "}
                 <Icon name="call" /> <b>Teléfono: </b>
                 {alumne.celular}
               </p>
               <p>
-                {" "}
                 <Icon name="calendar outline" />
                 <b>Fecha de nacimiento: </b>
                 {fecha_convertida}
               </p>
               <p>
-                {" "}
                 <Icon name="level up alternate" />
                 <b>Nivel de inglés: </b>
                 {alumne.nivelIngles}
               </p>
               <p>
-                {" "}
                 <Icon name="map marker alternate" />
                 <b>Sede: </b> {alumne.sede}
               </p>
               <p>
-                {" "}
                 <Icon name="map outline" />
                 <b>Nodo: </b>
                 {alumne.nodo}
