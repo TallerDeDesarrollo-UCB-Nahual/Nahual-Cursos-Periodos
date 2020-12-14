47facfc46f8b719fbe7bb7e42c5ac67941772023
IvySaskia <saskiaivy@outlook.com>
2020-12-14 00:36:10 -0400

    funcionalidad/notificacion-cuando-se-creo-curso-correctamente agregado d enotificacion cuando se elimino un curso

--- src/components/Cursos/eliminarCurso.jsx
+++ src/components/Cursos/eliminarCurso.jsx
@@ -1,13 +1,22 @@
 import React, { useReducer } from "react";
 import { Button, Icon } from 'semantic-ui-react'
 import { eliminarCursoDePeriodo } from "../../servicios/periodos";
+import  servicionotificacion  from "../../servicios/notificaciones";
 
 
 function EliminarCurso({ idPeriodo, idCurso }) {
 
+    const mostrarNotificacion = () => { 
+        servicionotificacion.mostrarMensajeExito(
+          `Curso eliminado con exito`,''
+        );
+    }
+
     const eliminarCursoDeAPI = (idPeriodo, idCurso) => {
         eliminarCursoDePeriodo(idPeriodo, idCurso);
+        mostrarNotificacion();
         window.location.replace("/periodos");
+        setTimeout(function () { window.location.reload();}, 5000); 
     }
 
     return (
